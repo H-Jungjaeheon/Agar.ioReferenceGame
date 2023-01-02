81cccc6110a628b91c2639fd71c781c206944faf
H-JungJaeHeon <jjhking222@naver.com>
2023-01-03 02:36:54 +0900

    A* 알고리즘을 이용한 플레이어 움직임 구현 완료
    A* 알고리즘을 이용한 플레이어 움직임 구현 완료

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -31,7 +31,13 @@ public class Node
     [Tooltip("장애물 무시하여 목표까지의 거리(가로 + 세로)")]
     public int tDistance;
     
-    public int priorityScore { get { return sDistance + tDistance; } } //priorityScore : sDistance + tDistance
+    public int priorityScore //이동 우선순위 점수 (priorityScore : sDistance + tDistance)
+    {
+        get
+        {
+            return sDistance + tDistance;
+        }
+    } 
 }
 
 public class Player : MonoBehaviour
@@ -41,8 +47,7 @@ public class Player : MonoBehaviour
     [Tooltip("이동속도")]
     public float speed;
 
-    [Tooltip("현재 크기")]
-    private float size;
+    float size; //현재 크기
 
     public float Size
     {
@@ -53,15 +58,12 @@ public class Player : MonoBehaviour
 
             sizeVector.x = size;
             sizeVector.y = size;
-            sizeVector.z = 1;
 
             transform.localScale = sizeVector;
         }
     }
 
-    private Vector3 sizeVector = new Vector3(0f, 0f, 1f); //크기 조정용 벡터
-
-    //private Vector2 targetPos; //현재 마우스로 찍은 목표 포지션
+    Vector3 sizeVector = new Vector3(0f, 0f, 1f); //크기 조정용 벡터
 
     [Tooltip("측정 가능한 최대 좌표값(음수 x, y)")]
     public Vector2Int bottomLeft;
@@ -78,23 +80,25 @@ public class Player : MonoBehaviour
     [Tooltip("최종 길 노드 리스트")]
     public List<Node> FinalNodeList;
 
-    [Tooltip("대각선 움직임 허용 여부")]
-    public bool allowDiagonal;
+    int sizeX; //현재 전체 탐색 범위의 X값
 
-    [Tooltip("코너 가로질러 가기 불가능 허용 여부")]
-    public bool dontCrossCorner;
+    int sizeY; //현재 전체 탐색 범위의 Y값
 
-    int sizeX, sizeY;
+    Node[,] NodeArray; //현재 전체 경로 탐색 범위(2차원 노드 배열)
 
-    Node[,] NodeArray;
+    Node StartNode; //시작(출발점) 노드
 
-    Node StartNode, TargetNode, CurNode;
+    Node TargetNode; //목표(도착점) 노드
 
-    List<Node> OpenList;
+    Node CurNode; //현재 위치 노드
 
-    List<Node> ClosedList;
+    List<Node> OpenList; //현재 진행 가능 경로의 노드들
 
-    Camera mainCam;
+    List<Node> ClosedList; //진행 완료된 경로의 노드들
+
+    IEnumerator movingCoroutine;
+
+    Camera mainCam; //메인 카메라 컴포넌트
 
     private void Start()
     {
@@ -104,7 +108,6 @@ public class Player : MonoBehaviour
     private void Update()
     {
         MouseInput();
-        Moving();
     }
 
     private void Init()
@@ -116,22 +119,23 @@ public class Player : MonoBehaviour
 
         mainCam = Camera.main;
 
-        size = 0.7f;
+        size = 0.65f;
     }
 
     private void MouseInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            //targetPos = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-            targetPos = Camera.main.ScreenToWorldPoint(new Vector3Int((int)Input.mousePosition.x, (int)Input.mousePosition.y, 0));
+            if (movingCoroutine != null)
+            {
+                StopCoroutine(movingCoroutine);
+            }
+
+            startPos = Vector2Int.RoundToInt(transform.position);
+            targetPos = Camera.main.ScreenToWorldPoint(new Vector3Int((int)Input.mousePosition.x, (int)Input.mousePosition.y, 10));
+            
             PathFinding();
         }
-    }
-
-    private void Moving()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
     }
 
     public void PathFinding() //길찾기 시작 함수
@@ -144,46 +148,44 @@ public class Player : MonoBehaviour
         topRight.y = (int)(transform.position.y + (5 + mainCam.orthographicSize));
         #endregion
 
-        startPos = Vector2Int.RoundToInt(transform.position); //만약 시작 지점 좌표가 소숫점이 포함된다면 int로 변형해서 가져오기(길찾기 시작 위치 현재 플레이어 위치로 초기화)
-
-        // NodeArray의 크기 정해주기(현재 길찾기 전체 범위)
-        sizeX = topRight.x - bottomLeft.x + 1; //1 더하는 이유는 0 포함
+        // NodeArray의 크기 정해주기(현재 길찾기 전체 범위, 1 더하는 이유는 0좌표를 포함하기 위함)
+        sizeX = topRight.x - bottomLeft.x + 1;
         sizeY = topRight.y - bottomLeft.y + 1;
 
         NodeArray = new Node[sizeX, sizeY]; //현재 탐색 범위 설정
 
-        for (int i = 0; i < sizeX; i++) //전체 범위를 탐색해서 벽 찾기
+        for (int i = 0; i < sizeX; i++) //전체 범위를 탐색해서 벽 찾기 (0,0 ~ sizeX-1, sizeY-1)
         {
             for (int j = 0; j < sizeY; j++)
             {
                 bool isWall = false;
 
-                foreach (Collider2D col in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f)) //좌표를 돌아가며 벽 판별용 원을 그린다(한 칸으로 잡은 범위만큼 원 생성)
+                foreach (Collider2D collider in Physics2D.OverlapCircleAll(new Vector2(i + bottomLeft.x, j + bottomLeft.y), 0.4f)) //좌표를 돌아가며 벽 판별용 원을 그린다(한 칸으로 잡은 범위만큼 원 생성)
                 {
-                    if (col.gameObject.CompareTag("Wall"))
+                    if (collider.gameObject.CompareTag("Wall"))
                     {
                         isWall = true;
                     }
                 }
 
-                NodeArray[i, j] = new Node(isWall, i + bottomLeft.x, j + bottomLeft.y); //isWall, x, y 대입
+                NodeArray[i, j] = new Node(isWall, i + bottomLeft.x, j + bottomLeft.y); //각 좌표의 노드 설정 : isWall, x, y 대입 (화면의 왼쪽 아래 노드 ~ 화면의 오른쪽 위 노드까지)
             }
         }
-
 
         // 시작과 끝 노드, 열린리스트와 닫힌리스트, 마지막리스트 초기화
         StartNode = NodeArray[startPos.x - bottomLeft.x, startPos.y - bottomLeft.y];
 
         TargetNode = NodeArray[(int)targetPos.x - bottomLeft.x, (int)targetPos.y - bottomLeft.y];
 
+        //길찾기 시작할 때 열린리스트에 시작지점 노드 넣기, 각 노드들 중복 방지 초기화 선언
         OpenList = new List<Node>() { StartNode };
         ClosedList = new List<Node>();
         FinalNodeList = new List<Node>();
 
-
         while (OpenList.Count > 0)
         {
-            // 열린리스트 중 가장 F가 작고 F가 같다면 H가 작은 걸 현재노드로 하고 열린리스트에서 닫힌리스트로 옮기기
+            //열린리스트 노드 중 F가 가장 작으면 해당 노드를 현재 방향 노드로 지정
+            //F가 같다면 H가 작은 걸 현재 방향 노드로 하고 열린리스트에서 닫힌리스트로 옮기기(H도 같다면 0번째 노드를 현재 방향 노드로 지정)
             CurNode = OpenList[0];
 
             for (int i = 1; i < OpenList.Count; i++)
@@ -210,24 +212,19 @@ public class Player : MonoBehaviour
                 }
 
                 FinalNodeList.Add(StartNode);
-                FinalNodeList.Reverse();
+                FinalNodeList.Reverse(); //반전시켜, 0번째부터 순차적으로 길 찾아가도록 하기
 
-                for (int i = 0; i < FinalNodeList.Count; i++)
-                {
-                    print(i + "번째는 " + FinalNodeList[i].xPos + ", " + FinalNodeList[i].yPos);
-                }
-                return;
+                movingCoroutine = Moving();
+                StartCoroutine(movingCoroutine);
+
+                break;
             }
-
 
             // ↗↖↙↘
-            if (allowDiagonal)
-            {
-                OpenListAdd(CurNode.xPos + 1, CurNode.yPos + 1);
-                OpenListAdd(CurNode.xPos - 1, CurNode.yPos + 1);
-                OpenListAdd(CurNode.xPos - 1, CurNode.yPos - 1);
-                OpenListAdd(CurNode.xPos + 1, CurNode.yPos - 1);
-            }
+            OpenListAdd(CurNode.xPos + 1, CurNode.yPos + 1);
+            OpenListAdd(CurNode.xPos - 1, CurNode.yPos + 1);
+            OpenListAdd(CurNode.xPos - 1, CurNode.yPos - 1);
+            OpenListAdd(CurNode.xPos + 1, CurNode.yPos - 1);
 
             // ↑ → ↓ ←
             OpenListAdd(CurNode.xPos, CurNode.yPos + 1);
@@ -242,30 +239,21 @@ public class Player : MonoBehaviour
         // 상하좌우 범위를 벗어나지 않고, 벽이 아니면서, 닫힌리스트에 없다면
         if (checkX >= bottomLeft.x && checkX < topRight.x + 1 && checkY >= bottomLeft.y && checkY < topRight.y + 1 && !NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y].isWall && !ClosedList.Contains(NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y]))
         {
-            // 대각선 허용시, 벽 사이로 통과 안됨
-            if (allowDiagonal)
+            if (NodeArray[CurNode.xPos - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.yPos - bottomLeft.y].isWall)
             {
-                if (NodeArray[CurNode.xPos - bottomLeft.x, checkY - bottomLeft.y].isWall && NodeArray[checkX - bottomLeft.x, CurNode.yPos - bottomLeft.y].isWall)
-                {
-                    return;
-                }
+                return;
             }
 
-            // 코너를 가로질러 가지 않을시, 이동 중에 수직수평 장애물이 있으면 안됨
-            if (dontCrossCorner)
+            if (NodeArray[CurNode.xPos - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.yPos - bottomLeft.y].isWall)
             {
-                if (NodeArray[CurNode.xPos - bottomLeft.x, checkY - bottomLeft.y].isWall || NodeArray[checkX - bottomLeft.x, CurNode.yPos - bottomLeft.y].isWall)
-                {
-                    return;
-                }
+                return;
             }
 
-            // 이웃노드에 넣고, 직선은 10, 대각선은 14비용
+            // 이웃노드에 넣고, 직선은 10, 대각선은 14만큼의 거리 지정
             Node NeighborNode = NodeArray[checkX - bottomLeft.x, checkY - bottomLeft.y];
             int MoveCost = CurNode.sDistance + (CurNode.xPos - checkX == 0 || CurNode.yPos - checkY == 0 ? 10 : 14);
 
-
-            // 이동비용이 이웃노드G보다 작거나 또는 열린리스트에 이웃노드가 없다면 G, H, ParentNode를 설정 후 열린리스트에 추가
+            // 이동비용이 이웃노드 sDistance보다 작거나 또는 열린리스트에 이웃노드가 없다면 sDistance, tDistance, ParentNode를 설정 후 열린리스트에 추가
             if (MoveCost < NeighborNode.sDistance || !OpenList.Contains(NeighborNode))
             {
                 NeighborNode.sDistance = MoveCost;
@@ -273,6 +261,29 @@ public class Player : MonoBehaviour
                 NeighborNode.ParentNode = CurNode;
 
                 OpenList.Add(NeighborNode);
+            }
+        }
+    }
+
+    IEnumerator Moving()
+    {
+        Vector2 targetPos;
+
+        for (int nowIndex = 0; nowIndex < FinalNodeList.Count; nowIndex++)
+        {
+            targetPos.x = FinalNodeList[nowIndex].xPos;
+            targetPos.y = FinalNodeList[nowIndex].yPos;
+
+            while (true)
+            {
+                if (transform.position.x == targetPos.x && transform.position.y == targetPos.y)
+                {
+                    break;
+                }
+
+                transform.position = Vector3.MoveTowards(transform.position, targetPos, Time.deltaTime * speed);
+
+                yield return null;
             }
         }
     }
